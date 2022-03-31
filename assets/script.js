3d3ae20bde942aab95c90ba90b8cf674079c45ea
ah-fang <94493319+ah-fang@users.noreply.github.com>
2022-03-31 15:01:05 -0700

    js conditional updates

--- assets/script.js
+++ assets/script.js
@@ -6,29 +6,46 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   //do stuff
-  window.prompt("Please enter the desired character length of your password as a number. (Note: password must be between 8 and 129 characters in length.");
-  /*if (input < 8 || input > 129) {
-    window.alert("Please choose a number between 8 and 129.");
-  }
-    */
-  
-  window.prompt("Would you like your password to have lowercase characters? If so, click OK. If not, click cancel.");
-  if(prompt) {
-    //use charset 
-    console.log("We've added the lowercase characters, captain.")
-  }
-  window.prompt("Would you like your password to have uppercase characters? If so, click OK. If not, click cancel.");
-  window.prompt("Would you like your password to have numeric characters? If so, click OK. If not, click cancel.");
-  window.prompt("Would you like your password to have special characters? If so, click OK. If not, click cancel.");
+  function numberCheck() {
+    var length = window.prompt("Please enter the desired character length of your password as a number. (Note: password must be between 8 and 129 characters in length.");
+    if (length < 8 || length > 129) {
+      window.alert("Please choose a number between 8 and 129.");
+      numberCheck();
+      //prompt length again. Do not move on to other prompts! 
+    }
+  } 
+   if (length >= 8 && length <= 129) {
+    console.log("The password will be " + length + " characters long.")  
+    console.log("This number is acceptable.")
+    }
+//   if(window.confirm("Would you like your password to have lowercase characters? If so, click OK. If not, click cancel.")) {
+//     console.log("We've added the lowercase characters, captain.")
+//     //then add charset 
+//   }
+//  else {
+//     console.log("It's been an honor.");
+//   }
+//   window.prompt("Would you like your password to have uppercase characters? If so, click OK. If not, click cancel.");
+//   window.prompt("Would you like your password to have numeric characters? If so, click OK. If not, click cancel.");
+//   window.prompt("Would you like your password to have special characters? If so, click OK. If not, click cancel.");
 }
 
+function upperCase(){
+  //charset for uppercase letters
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return upper[Math.floor(Math.random() * upper.length)]; 
+}
+function lowerCase(){
+  //charset for uppercase letters
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  return lower[Math.floor(Math.random() * lower.length)]; 
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   console.log("The function writePassword ran.")
   passwordText.value = password;
-
 }
 
 /*
@@ -47,9 +64,7 @@ function writePassword() {
   else {
     window.alert("You must select at least one character set.");
     reset options and ask again.
-  }
-
-  
+  }  
 */
 
 // Add event listener to generate button
